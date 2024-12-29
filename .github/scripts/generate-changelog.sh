c685727ef164f50edc22f98723ea7eb57ba8b5f4
Louan Fontenele <louanbastos61@gmail.com>
2024-12-29 06:10:46 -0300

    Feature: Updated Changelog

--- .github/scripts/generate-changelog.sh
+++ .github/scripts/generate-changelog.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-set -e
 
-LATEST_TAG=${previous_tag}
-NEW_TAG=${current_tag}
-
-echo "Generating changelog from $LATEST_TAG to $NEW_TAG"
+LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+NEW_TAG=$(git describe --tags --abbrev=0 HEAD)
 
 echo "## What's Changed" > changelog.md
 echo "" >> changelog.md
 
-# Coletar todos os commits entre as tags
-git log $LATEST_TAG..HEAD --pretty=format:"- %s by @%an" >> changelog.md
+git log --pretty=format:"- %s by @%an" $LATEST_TAG..HEAD >> changelog.md
 
 echo "" >> changelog.md
-echo "## 📦 Full Changelog" >> changelog.md
-
-# Corrigido para usar a variável do GitHub corretamente
-REPO_URL="https://github.com/${GITHUB_REPOSITORY}"
-echo "**Full commit history:** [Compare Changes](${REPO_URL}/compare/$LATEST_TAG...$NEW_TAG)" >> changelog.md
+echo "**Full commit history:** [Compare Changes](https://github.com/$GITHUB_REPOSITORY/compare/$LATEST_TAG...$NEW_TAG)" >> changelog.md
