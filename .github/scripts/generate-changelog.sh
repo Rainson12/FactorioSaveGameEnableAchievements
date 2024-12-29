db87d40805be15a0151f12ff6c50e5712b9e001a
Louan Fontenele <louanbastos61@gmail.com>
2024-12-29 05:48:10 -0300

    Fix: Changelogs

--- .github/scripts/generate-changelog.sh
+++ .github/scripts/generate-changelog.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-
 LATEST_TAG=$(git describe --tags --abbrev=0)
-echo "Generating changelog since $LATEST_TAG..."
-
 echo "## 🚀 New Features" > changelog.md
 git log $LATEST_TAG..HEAD --grep "Feature:" --pretty=format:"- %s by @%an" >> changelog.md
+echo "" >> changelog.md
 
-echo -e "\n## 🐛 Bug Fixes" >> changelog.md
+echo "## 🐛 Bug Fixes" >> changelog.md
 git log $LATEST_TAG..HEAD --grep "Fix:" --pretty=format:"- %s by @%an" >> changelog.md
+echo "" >> changelog.md
 
-echo -e "\n## 🛠️ Chores" >> changelog.md
-git log $LATEST_TAG..HEAD --grep "Chore:" --pretty=format:"- %s by @%an" >> changelog.md
-
-echo -e "\nChangelog generated successfully! 🎉"
+echo "## 🛠️ Maintenance" >> changelog.md
+git log $LATEST_TAG..HEAD --grep "chore:" --pretty=format:"- %s by @%an" >> changelog.md
+echo "" >> changelog.md
