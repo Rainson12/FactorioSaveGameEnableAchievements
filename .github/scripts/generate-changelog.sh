23b7aa5f2e7acd5434bd0414e7b7b8c58be6b0ea
Louan Fontenele <louanbastos61@gmail.com>
2024-12-29 05:55:48 -0300

    Fix: Hope changelog are fixed now!

--- .github/scripts/generate-changelog.sh
+++ .github/scripts/generate-changelog.sh
@@ -1,13 +1,29 @@
-# .github/scripts/generate-changelog.sh
-# Generate changelog sections by commit type
+#!/bin/bash
+
+# Fetch the latest tag
+LATEST_TAG=$(git describe --tags --abbrev=0)
+
+# If no tag is found, set default
+if [ -z "$LATEST_TAG" ]; then
+  LATEST_TAG=$(git rev-list --max-parents=0 HEAD)
+fi
+
+# Generate Changelog
 echo "## 🚀 New Features" > changelog.md
-git log $(git describe --tags --abbrev=0 @^)..@ --grep "Feature:" --pretty=format:"- %s by @%an" >> changelog.md
+git log $LATEST_TAG..HEAD --grep "Feature:" --pretty=format:"- %s by @%an" >> changelog.md
 echo "" >> changelog.md
 
 echo "## 🐛 Bug Fixes" >> changelog.md
-git log $(git describe --tags --abbrev=0 @^)..@ --grep "Fix:" --pretty=format:"- %s by @%an" >> changelog.md
+git log $LATEST_TAG..HEAD --grep "Fix:" --pretty=format:"- %s by @%an" >> changelog.md
 echo "" >> changelog.md
 
 echo "## 🛠️ Maintenance" >> changelog.md
-git log $(git describe --tags --abbrev=0 @^)..@ --grep "chore:" --pretty=format:"- %s by @%an" >> changelog.md
+git log $LATEST_TAG..HEAD --grep "Chore:" --pretty=format:"- %s by @%an" >> changelog.md
 echo "" >> changelog.md
+
+echo "## 🧩 Dependencies" >> changelog.md
+git log $LATEST_TAG..HEAD --grep "Dep:" --pretty=format:"- %s by @%an" >> changelog.md
+echo "" >> changelog.md
+
+# Display the generated changelog
+cat changelog.md
