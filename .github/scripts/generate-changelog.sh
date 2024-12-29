ec54487fe19d23c0c90db25daec9d5cbfae6d1f6
Louan Fontenele <louanbastos61@gmail.com>
2024-12-29 06:56:46 -0300

    Fix: Changelog export issue

--- .github/scripts/generate-changelog.sh
+++ .github/scripts/generate-changelog.sh
@@ -31,4 +31,4 @@ fi
 
 echo "" >> $CHANGELOG_FILE
 REPO_URL="https://github.com/${GITHUB_REPOSITORY}/compare/"
-echo "**Full commit history:** [Compare Changes](${REPO_URL}${LATEST_TAG}...${NEW_TAG})" >> $CHANGELOG_FILE
+echo "**\n\nFull commit history:** [Compare Changes](${REPO_URL}${LATEST_TAG}...${NEW_TAG})" >> $CHANGELOG_FILE
