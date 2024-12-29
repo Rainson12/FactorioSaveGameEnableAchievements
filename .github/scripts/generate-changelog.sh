7962353febe6c9a16ce2c2b8b915e63de7a76bba
Louan Fontenele <louanbastos61@gmail.com>
2024-12-29 06:24:45 -0300

    Feature: Add new changelog system
    Fix:
    - Correct changelog formatting
    Feature:
    - Implement changelog from commit description

--- .github/scripts/generate-changelog.sh
+++ .github/scripts/generate-changelog.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-LATEST_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
-NEW_TAG=$(git describe --tags --abbrev=0 HEAD)
+# Get previous and current tags
+LATEST_TAG=$(git tag --sort=-v:refname | grep "^2.0.28-" | head -n 1)
+NEW_TAG=$(echo "$LATEST_TAG" | awk -F '-' '{printf "%s-%0.1f", $1, $2 + 0.1}')
 
+# Generate the changelog content
 echo "## What's Changed" > changelog.md
 echo "" >> changelog.md
-
 git log --pretty=format:"- %s by @%an" $LATEST_TAG..HEAD >> changelog.md
-
 echo "" >> changelog.md
-echo "**Full commit history:** [Compare Changes](https://github.com/$GITHUB_REPOSITORY/compare/$LATEST_TAG...$NEW_TAG)" >> changelog.md
+
+# Append Full commit history section
+echo "**Full commit history:** [Compare Changes](https://github.com/${GITHUB_REPOSITORY}/compare/$LATEST_TAG...$NEW_TAG)" >> changelog.md
