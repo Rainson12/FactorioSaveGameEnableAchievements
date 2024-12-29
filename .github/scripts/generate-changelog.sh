1baf96a4548436d1d3a9e1df8740878ec685b55d
Louan Fontenele <louanbastos61@gmail.com>
2024-12-29 05:51:08 -0300

    Fix: Try to fix Changelogs

--- .github/scripts/generate-changelog.sh
+++ .github/scripts/generate-changelog.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-LATEST_TAG=$(git describe --tags --abbrev=0)
+# .github/scripts/generate-changelog.sh
+# Generate changelog sections by commit type
 echo "## 🚀 New Features" > changelog.md
-git log $LATEST_TAG..HEAD --grep "Feature:" --pretty=format:"- %s by @%an" >> changelog.md
+git log $(git describe --tags --abbrev=0 @^)..@ --grep "Feature:" --pretty=format:"- %s by @%an" >> changelog.md
 echo "" >> changelog.md
 
 echo "## 🐛 Bug Fixes" >> changelog.md
-git log $LATEST_TAG..HEAD --grep "Fix:" --pretty=format:"- %s by @%an" >> changelog.md
+git log $(git describe --tags --abbrev=0 @^)..@ --grep "Fix:" --pretty=format:"- %s by @%an" >> changelog.md
 echo "" >> changelog.md
 
 echo "## 🛠️ Maintenance" >> changelog.md
-git log $LATEST_TAG..HEAD --grep "chore:" --pretty=format:"- %s by @%an" >> changelog.md
+git log $(git describe --tags --abbrev=0 @^)..@ --grep "chore:" --pretty=format:"- %s by @%an" >> changelog.md
 echo "" >> changelog.md
