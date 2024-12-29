19cef85e3ad302d09a8451237aa6bc10e1402df1
Louan Fontenele <louanbastos61@gmail.com>
2024-12-29 08:54:52 -0300

    Feature: Generate and edit a new README from scratch

--- .github/scripts/update-readme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-REPO_OWNER=$(echo $GITHUB_REPOSITORY | cut -d'/' -f1)
-REPO_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f2)
-
-# Atualiza o badge e links
-sed -i "s|OWNER/REPOSITORY|$REPO_OWNER/$REPO_NAME|g" README.md
-sed -i "s|\${{ github.repository }}|$GITHUB_REPOSITORY|g" README.md
-sed -i "s|\${{ github.repository_owner }}|$REPO_OWNER|g" README.md
